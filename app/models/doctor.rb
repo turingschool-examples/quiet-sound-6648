8860e6ba0ccc0f37b0e21e80d35cc66294f27976
Evan Davis <evandavis427@gmail.com>
2024-08-06 09:29:58 -0600

    initial setup

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,7 @@
 class Doctor < ApplicationRecord
   belongs_to :hospital
+  has_many :doctor_patients
+  has_many :patients, through: :doctor_patients
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+DoctorPatient.destroy_all
+Doctor.destroy_all
+Patient.destroy_all
+Hospital.destroy_all
+
+# Hospitals
+hospital1 = Hospital.create!(name: 'Sacred Heart')
+hospital2 = Hospital.create!(name: 'Seattle Grace')
+
+# Doctors
+doctor1 = hospital1.doctors.create!(name: 'Dr. Cox', specialty: 'Sarcasm', university: 'Harvard')
+doctor2 = hospital1.doctors.create!(name: 'Dr. Turk', specialty: 'Surgery', university: 'Johns Hopkins')
+doctor3 = hospital2.doctors.create!(name: 'Dr. House', specialty: 'Diagnostic Medicine', university: 'Princeton')
+doctor4 = hospital2.doctors.create!(name: 'Dr. Wilson', specialty: 'Oncology', university: 'Yale')
+
+# Patients
+patient1 = Patient.create!(name: 'John Doe', age: 39)
+patient2 = Patient.create!(name: 'Jane Doe', age: 28)
+patient3 = Patient.create!(name: 'Jimmy Neutron', age: 17)
+patient4 = Patient.create!(name: 'Timmy Turner', age: 20)
+patient5 = Patient.create!(name: 'Carl Wheezer', age: 25)
+
+# Doctor-Patient Relationships
+DoctorPatient.create!(doctor: doctor1, patient: patient1)
+DoctorPatient.create!(doctor: doctor1, patient: patient2)
+DoctorPatient.create!(doctor: doctor2, patient: patient3)
+DoctorPatient.create!(doctor: doctor3, patient: patient1)
+DoctorPatient.create!(doctor: doctor3, patient: patient4)
+DoctorPatient.create!(doctor: doctor4, patient: patient5)
+DoctorPatient.create!(doctor: doctor4, patient: patient2)
